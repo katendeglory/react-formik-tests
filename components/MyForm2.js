ac03f885cfb56ecfa263f7658ec8804f040a871d
katendeglory <katendeglory@gmail.com>
2019-12-16 15:48:39 +0200

    1 file modified

--- components/MyForm2.js
+++ components/MyForm2.js
@@ -6,8 +6,6 @@ import { Button, FormControlLabel , Radio} from "@material-ui/core";
 
 const MyRadio = ({label, ...props}) => {
   const [field, meta] = useField(props);
-  // console.log(props);
-  // console.log(field);
   return <FormControlLabel {...field} control={<Radio/>} label={label} />
 }
 
