ad4b7cfffaf6d59e7e7f350c0b0834d8943b68ca
katendeglory <katendeglory@gmail.com>
2019-12-16 11:38:25 +0200

    1 file modified

--- components/MyForm.js
+++ components/MyForm.js
@@ -1,15 +1,13 @@
 import React from "react";
-import { Formik, Field } from "formik";
+import { Formik, Form, Field } from "formik";
 import { TextField, Button } from "@material-ui/core";
 
 const MyForm = () => {
   const initValues = { firstName: "", lastName: "" };
 
-  const submitForm = (data, {resetForm, setSubmitting}) => {
-    console.log(data);
-
+  const submitForm = (values, {resetForm, setSubmitting}) => {
+    console.log(values);
     setSubmitting(true);
-
     setTimeout(() => {
       setSubmitting(false);
       resetForm({});
@@ -19,17 +17,15 @@ const MyForm = () => {
   return (
     <div>
       <Formik initialValues={initValues} onSubmit={submitForm}>
-        {({ values, handleChange, handleBlur, handleSubmit, isSubmitting, setFieldValue }) => (
-          <form onSubmit={handleSubmit}>
-
+        {({ values, isSubmitting, setFieldValue }) => (
+          <Form>
             <Field name="firstName" as={TextField} placeholder="First Name" /><br/><br/>
-            <Field name="lastName" as={TextField} placeholder="Last Name" />
- 
-            <br/><br/>
-            <Button type="submit" variant="contained" color="primary" disabled={isSubmitting}>Submit</Button>
-            <br/><br/>
+            <Field name="lastName" as={TextField} placeholder="Last Name" /><br/><br/>
+            
+            <Button type="submit" variant="contained" disabled={isSubmitting}>Submit</Button><br/><br/>
+          
             <pre>{JSON.stringify(values, null, 2)}</pre>
-          </form>
+          </Form>
         )}
       </Formik>
     </div>
