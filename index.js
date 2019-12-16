7f0d6da6d40349a4c2f54b67ddfd0f61c54b8b31
katendeglory <katendeglory@gmail.com>
2019-12-16 18:23:51 +0200

    1 file modified

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import ReactDOM from 'react-dom';
-import MyForm from "./components/MyForm2";
+import Form from "./components/MyForm2";
 import "./style.css";
 
 const App = () => {
   return (
     <div>
-      <MyForm />
+      <Form />
     </div>
   );
 };
