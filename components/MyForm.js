06891c639bfe9e2b14126889520ceac4e5c4498a
katendeglory <katendeglory@gmail.com>
2019-12-16 11:56:24 +0200

    1 file modified

--- components/MyForm.js
+++ components/MyForm.js
@@ -3,7 +3,7 @@ import { Formik, Form, Field } from "formik";
 import { TextField, Button, Checkbox } from "@material-ui/core";
 
 const MyForm = () => {
-  const initValues = { firstName: "", lastName: "", getEmails: true };
+  const initValues = { firstName: "", lastName: "", getEmails: true, cookies: [] };
 
   const submitForm = (values, {resetForm, setSubmitting}) => {
     console.log(values);
@@ -22,8 +22,13 @@ const MyForm = () => {
             <Field type="text" name="firstName" as={TextField} placeholder="First Name" /><br/><br/>
             <Field type="text" name="lastName" as={TextField} placeholder="Last Name" /><br/><br/>
             <Field type="checkbox" name="getEmails" as={Checkbox} />Get Emails<br/><br/>
+
+            <div>Cookies:</div>
+            <Field type="checkbox" name="cookies" as={Checkbox} value="Chocolate Chips" />Chocolate Chips<br/>
+            <Field type="checkbox" name="cookies" as={Checkbox} value="Sneaker Doodle" />Sneaker Doodle<br/>
+            <Field type="checkbox" name="cookies" as={Checkbox} value="Candy King" />Candy King<br/>
+
             <Button type="submit" variant="contained" disabled={isSubmitting}>Submit</Button><br/><br/>
-          
             <pre>{JSON.stringify(values, null, 2)}</pre>
           </Form>
         )}
