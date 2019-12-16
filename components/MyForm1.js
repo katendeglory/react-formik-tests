b777ccc0e38e459c5ce37a9deea82d928d85e53c
katendeglory <katendeglory@gmail.com>
2019-12-16 12:18:28 +0200

    1 file modified, 2 added, 1 deleted

---
+++ components/MyForm1.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Formik, Form } from "formik";
+
+const App = () => {
+  const initValues = { file: null };
+
+  const onSubmit = (values, { resetForm, setSubmitting }) => {
+    console.log(values);
+  };
+
+  return (
+    <div className="container">
+      <Formik initialValues={initValues} onSubmit={onSubmit}>
+        {({ values, setFieldValue }) => (
+          <Form>
+            <input name="file" type="file" onChange={e => setFieldValue("file", e.currentTarget.files[0])}/>
+            <button type="submit">submit</button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
