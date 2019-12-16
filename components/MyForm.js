15dcd518770f023877d8e42ccbe1ba583dcedaf5
katendeglory <katendeglory@gmail.com>
2019-12-16 11:47:03 +0200

    1 file modified

--- components/MyForm.js
+++ components/MyForm.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Formik, Form, Field } from "formik";
-import { TextField, Button } from "@material-ui/core";
+import { TextField, Button, Checkbox } from "@material-ui/core";
 
 const MyForm = () => {
-  const initValues = { firstName: "", lastName: "" };
+  const initValues = { firstName: "", lastName: "", getEmails: true };
 
   const submitForm = (values, {resetForm, setSubmitting}) => {
     console.log(values);
@@ -19,9 +19,9 @@ const MyForm = () => {
       <Formik initialValues={initValues} onSubmit={submitForm}>
         {({ values, isSubmitting, setFieldValue }) => (
           <Form>
-            <Field name="firstName" as={TextField} placeholder="First Name" /><br/><br/>
-            <Field name="lastName" as={TextField} placeholder="Last Name" /><br/><br/>
-            
+            <Field type="text" name="firstName" as={TextField} placeholder="First Name" /><br/><br/>
+            <Field type="text" name="lastName" as={TextField} placeholder="Last Name" /><br/><br/>
+            <Field type="checkbox" name="getEmails" as={Checkbox} />Get Emails<br/><br/>
             <Button type="submit" variant="contained" disabled={isSubmitting}>Submit</Button><br/><br/>
           
             <pre>{JSON.stringify(values, null, 2)}</pre>
